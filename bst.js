b035493d0155a1e1d9461960846a4a8dba6b5dfb
alexwittwer <wittweral@gmail.com>
2023-12-05 20:51:18 -0500

    initialize setup

---
+++ bst.js
@@ -0,0 +1,30 @@
+class Tree {
+  constructor(inputArray) {
+    this.root = this.buildTree(inputArray);
+  }
+
+  buildTree(arr) {
+    // build tree
+  }
+}
+
+class Node {
+  constructor(value = null, left = null, right = null) {
+    this.value = value;
+    this.left = left;
+    this.right = right;
+  }
+}
+
+const prettyPrint = (node, prefix = "", isLeft = true) => {
+  if (node === null) {
+    return;
+  }
+  if (node.right !== null) {
+    prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+  }
+  console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+  if (node.left !== null) {
+    prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+  }
+};
